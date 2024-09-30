c725eb28a42e04a9e1955a021ac4834a8cebe270
Tang Cheng <cheng.tang@supwisdom.com>
2024-09-30 14:15:44 +0800

    fix error message

--- ndef/src/record.rs
+++ ndef/src/record.rs
@@ -1,6 +1,6 @@
-use crate::payload::*;
+use crate::{payload::*, error::NdefError};
 use crate::*;
-use anyhow::{anyhow, Result};
+use anyhow::anyhow;
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use std::io::{prelude::*, Cursor};
 
@@ -113,26 +113,26 @@ impl NdefRecord {
     pub(crate) fn decode(reader: &mut dyn Read) -> Result<Self> {
         let flags = reader.read_u8().map_err(|e| anyhow!("read error, {}", e))?;
         let tnf = TNF::from_repr(flags & 0x0f)
-            .ok_or_else(|| anyhow!("unknown tnf <{:02x}>", (flags & 0x0f)))?;
+            .ok_or_else(|| NdefError::InvalidTnf)?;
         let flags = RecordFlags::from_bits_retain(flags);
 
         let type_len = reader
             .read_u8()
-            .map_err(|_| anyhow!("read type length error"))?;
+            .map_err(|_| NdefError::InvalidTagLength)?;
         let payload_len = if flags & RecordFlags::SR == RecordFlags::SR {
             reader
                 .read_u8()
-                .map_err(|_| anyhow!("read payload length error"))? as u32
+                .map_err(|_| NdefError::InvalidPayload)? as u32
         } else {
             reader
                 .read_u32::<LittleEndian>()
-                .map_err(|_| anyhow!("read payload length error"))?
+                .map_err(|_| NdefError::InvalidPayload)?
         };
 
         let id_len = if flags & RecordFlags::IL == RecordFlags::IL {
             reader
                 .read_u8()
-                .map_err(|_| anyhow!("read id length error"))?
+                .map_err(|_| NdefError::InvalidId)?
         } else {
             0
         };
@@ -140,13 +140,13 @@ impl NdefRecord {
         let mut record_type = vec![0u8; type_len as usize];
         reader
             .read_exact(&mut record_type)
-            .map_err(|_| anyhow!("read record type error"))?;
+            .map_err(|_| NdefError::InvalidRecordType)?;
 
         let id = if id_len > 0 {
             let mut id = vec![0u8; id_len as usize];
             reader
                 .read_exact(&mut id)
-                .map_err(|_| anyhow!("read record id error"))?;
+                .map_err(|_| NdefError::InvalidId)?;
             Some(id)
         } else {
             None
@@ -155,7 +155,7 @@ impl NdefRecord {
         let mut payload = vec![0u8; payload_len as usize];
         reader
             .read_exact(&mut payload)
-            .map_err(|_| anyhow!("read payload error"))?;
+            .map_err(|_| NdefError::InvalidPayload)?;
         Ok(Self {
             flags,
             tnf,
@@ -217,7 +217,7 @@ impl NdefRecordBuilder {
         if self.tnf == TNF::Empty
             && (!self.payload.is_empty() || !self.record_type.is_empty() || self.id.is_some())
         {
-            return Err(anyhow!("Invalid empty record"));
+            return Err(anyhow!("Invalid empty record").into());
         }
         if self.tnf == TNF::Empty {
             Ok(NdefRecord {
@@ -229,11 +229,11 @@ impl NdefRecordBuilder {
             })
         } else {
             if self.record_type.len() > 0xff {
-                return Err(anyhow!("record type too long"));
+                return Err(anyhow!("record type too long").into());
             }
             if let Some(id) = self.id.as_ref() {
                 if id.len() > 0xff {
-                    return Err(anyhow!("record id too long"));
+                    return Err(anyhow!("record id too long").into());
                 }
             }
 
